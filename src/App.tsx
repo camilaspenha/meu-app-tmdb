c17405e9a274fae460fccf1fd0155dd96393623b
Camila Silva da Penha <camilaspenha@gmail.com>
2025-07-30 20:58:33 -0300

    rotas e vite config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Favoritos from "./pages/Favoritos"
 import Inicio from "./pages/Inicio"
-
+import './styles/custom.css'
 import {SITE_URL} from './constants/dados'
 
 function App() {
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './styles/custom.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
