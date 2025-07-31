7d57feb4620ed60cbb08ceead7a09d014ad42053
Camila Silva da Penha <camilaspenha@gmail.com>
2025-07-31 07:24:39 -0300

    fix com pag 404

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/custom.css'
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/meu-app-tmdb/">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
